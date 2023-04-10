08f36244a3408f16a3b356cd27534f7f7e3a3cd5
Kim Almroth <almroth.kim@gmail.com>
2023-04-10 10:16:41 +0200

    add nodejs frontend

---
+++ Frontend/public/js/user/seat.js
@@ -0,0 +1,60 @@
+import {getJwtPayload, login, verifyJwt} from "../admin/auth.js";
+
+window.addEventListener('load', async ev => {
+    await login();
+    const statusCode = await verifyJwt();
+    console.log(statusCode)
+    if (statusCode !== 200) {
+        window.location.assign('http://localhost:3000/unauthorized')
+    } else {
+        await setName();
+        const seat = await getSeat();
+        console.log(seat)
+        fillSeat(seat);
+
+
+    }
+})
+
+const getSeat = async () => {
+    const uuid = '65ff0289-9e00-4574-9f34-8f24b9a03ef4';
+    const url = 'http://83.233.216.66:35462/api/admin/seat/' + uuid;
+    const token = window.localStorage.getItem("jwt");
+    let res;
+
+    await fetch(url, {
+        method: 'GET',
+        headers: {
+            "Authorization": "Bearer " + token,
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                res = response.json()
+            } else throw Error("Error getting seat: " + response.status)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    return res;
+}
+
+async function setName() {
+
+    const data = await getJwtPayload();
+    document.getElementById("account").innerText += data['sub'];
+
+}
+
+const fillSeat = (seat) => {
+
+    document.getElementById('for').innerText = `${seat.businessName} - ${seat.forYearMonth}`;
+
+    document.getElementById("uuid").innerText = seat.uuid;
+    document.getElementById("businessName").innerText = seat.businessName;
+    document.getElementById("completedByEmail").innerText = (seat.completedByEmail) ?? 'Ej ifylld';
+    document.getElementById("isCompleted").innerText = (seat.isCompleted) ? 'Ja' : 'Nej';
+    document.getElementById("lastChangeDate").innerText = (seat.lastChangeDate ?? 'Har ej ändrats');
+    document.getElementById("seatUsed").innerText = (seat.seatUsed ?? 'Seat har inte fyllts i än');
+    document.getElementById("forYearMonth").innerText = seat.forYearMonth;
+}
