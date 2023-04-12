328165bce7c519899aa01bc780175eacec5d5d51
kalm <kim.almroth@apendo.se>
2023-04-12 11:49:58 +0200

    update works

--- User API/src/main/java/almroth/kim/gamendo_user_api/business/dto/UpdateBusinessRequest.java
+++ User API/src/main/java/almroth/kim/gamendo_user_api/business/dto/UpdateBusinessRequest.java
@@ -2,7 +2,10 @@ package almroth.kim.gamendo_user_api.business.dto;
 
 import almroth.kim.gamendo_user_api.accountProfile.model.AccountProfile;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 import java.util.UUID;
@@ -11,8 +14,6 @@ import java.util.UUID;
 public class UpdateBusinessRequest {
 
     private String name;
-
-    private Set<AccountProfile> accountProfiles;
     private Integer seatAmount;
     private String accountUUID;
 }
