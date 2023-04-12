221b9de30adcb3e0b74f9fcabb879e59f537acc1
Kim Almroth <90030358+KimAlmh@users.noreply.github.com>
2023-04-12 14:02:20 +0200

    added back removed code

--- Frontend/public/js/admin/business.js
+++ Frontend/public/js/admin/business.js
@@ -11,5 +11,26 @@ function getBusiness() {
         'Authorization': `Bearer ${window.localStorage.getItem("jwt")}`,
         "Content-Type": "application/json",
       },
+      })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const tableBody = document.getElementById("myTable");
+      // remove any existing rows from the table
+      //  tableBody.innerHTML = '';
+      // loop through the data and create a new row for each object
+      data.forEach(obj => {
+        const row = document.createElement('tr');
+        const uuidCell = document.createElement('td');
+        const nameCell = document.createElement('td');
+        const seatAmountCell = document.createElement('td');
+        uuidCell.innerText = obj.uuid;
+        nameCell.innerText = obj.name;
+        seatAmountCell.innerText = obj.seatBaseline;
+        row.appendChild(uuidCell);
+        row.appendChild(nameCell);
+        row.appendChild(seatAmountCell);
+        tableBody.appendChild(row);
+      });
     })
 }
